d8837e7227ea3f9a4f3f864f9ee9d04971c81662
ChilaLohith <lohithchila007@gmail.com>
2022-06-23 10:54:52 +0530

    Using VueJS
    Claranet assessment using VueJS.

---
+++ claranet-assessment-using-vue/claranet/src/App.vue
@@ -0,0 +1,60 @@
+<script setup>
+import Welcome from './components/Welcome.vue'
+import BeersTable from './components/BeersTable.vue'
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <Welcome msg="Welcome to BEERS" />
+    </div>
+  </header>
+
+  <main>
+    <div class="table-wrapper">
+      <BeersTable />
+    </div>
+  </main>
+
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  margin: 0 auto;
+  font-weight: normal;
+}
+
+header {
+  background-color: #26302E;
+  line-height: 1.5;
+  position: fixed;
+  padding-top: 15px;
+  width: 100%;
+  z-index: 1;
+}
+
+main {
+  max-width: 1280px;
+  position: relative;
+  top: 100px;
+  left: 8%;
+  width: 100%;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 1);
+  }
+}
+</style>
+
---
+++ claranet-assessment-using-vue/claranet/src/components/Welcome.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+.greetings h1 {
+  text-align: center;
+}
+
+</style>
